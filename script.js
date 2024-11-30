d9e600fe11c8a1176c22f8a8fe4a077f738d26e3
96bbyy <96bbyyproductions@gmail.com>
2024-11-29 20:39:38 -0500

    11-29-24 Quotes working

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log('Testing')
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+let apiQuotes = []
+function newQuote() {
+    // pick a random quote from apiQuote array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';}
+    else {authorText.textContent = quote.author;}
+    // Check quote Length to determine the styling
+    if (quote.text.length > 150){
+        quoteText.classList.add('long-quote');}
+    else {quoteText.classList.remove('long-quote');}
+    console.log(quote.text);
+    console.log(quote.author);
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
+}
+// Get Quotes From API
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        // using async function and the await keyword means
+        // response will remain empty until it is populated with the response from api
+        // otherwise there would be an error because response would be filled before the 
+        // request is complete
+        const response = await fetch(apiUrl);
+        // check if author field is blank and replace with "unknow"
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // catch error Here
+    }
+}
+// Tweet Button = tweet quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+//Event Listeners
+newQuoteBtn.addEventListener('click',newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+// On Load
+getQuotes();
